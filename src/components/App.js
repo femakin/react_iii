391fcfe3cb3253ae063a86015c4949e6faa1e6f5
Femi Akinyemi <akinfemi46@gmail.com>
2020-09-04 11:03:43 +0100

    update stylesheet

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './stylesheet.css'
+
+
+ class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>I love react</h1>
+        <img src='https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/character_logo.svg' alt="" />
+      </div>
+    )
+  }
+}
+
+export default App
